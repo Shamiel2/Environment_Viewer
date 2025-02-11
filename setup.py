d976b266636429e262850fd972c51091299bd82f
Shamiel Rinquest <62132164+Shamiel2@users.noreply.github.com>
2025-02-11 20:12:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,7 @@ import setuptools
 setuptools.setup(
     name="environment_viewer",
     version="0.1.0",
-<<<<<<< HEAD
-    description="A Tool were you can View, Create and Modify your Environment Variables with a given DCC.",
-=======
     description="A Tool to View, Create and Modify your Environment Variables with a given DCC.",
->>>>>>> 22f1b0712fd1dffe270c97777d3cded7afb3d521
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
